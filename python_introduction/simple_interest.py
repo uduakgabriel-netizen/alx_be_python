0c5e939275bb3896b67da3d360b1dd0c1558dc32
uduakgabriel-netizen <uduakgabriel507@gmail.com>
2025-05-24 19:21:50 +0100

    simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,7 +2,7 @@ principal = 1000
 rate = 0.05
 time = 3
 
-interest = 1000 * 0.05 * 3
+interest = principal *rate * time 
 
-print(f" Multiplication of {principal} and {rate} and {time} is {interest}")
+print(f"The  simple interest is : {interest}")
 
