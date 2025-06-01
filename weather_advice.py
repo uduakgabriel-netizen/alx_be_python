5d3f66e01bc795e0a3ca889844aeb29edf7fc9b1
uduakgabriel-netizen <clea>
2025-06-01 11:34:20 -0700

    Weather Recommendation Program

---
+++ weather_advice.py
@@ -0,0 +1,17 @@
+weather = input ("what today weather look like? (sunny/rainny/cold):") .lower()
+
+if weather == "sunny":
+    
+    print("Wear a t-shirt and sunglasses.")
+    
+elif weather == "rainny":
+    
+    print(" Don't forget your umbrella and a raincoat.")
+    
+elif weather == " cold":
+    
+    print("Make sure to wear a warm coat and a scarf.")
+    
+else:
+    
+    print("Sorry, I don't have recommendations for this weather. ")
