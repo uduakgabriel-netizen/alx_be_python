bf1418fc427edfd90aaf37d3276836c99a071a69
uduak gabriel <uduakgabriel507@gmail.com>
2025-06-04 07:51:02 -0700

    multiplication calculator

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,3 +1,5 @@
-x = int(input("Enter a number to see its multiplication table:"))
+x = int(input("Enter a number to see its multiplication table: ")) 
 
-
+for y in range (1, 11):
+    z = x * y
+    print(f"{x}  * {y} ={z}" )
