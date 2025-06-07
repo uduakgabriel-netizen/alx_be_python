aad6f73bbe78178b46ad6823fffc3008c680cfdd
uduak gabriel <uduakgabriel507@gmail.com>
2025-06-07 08:24:18 -0700

    changes in the operations signs

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,7 +3,7 @@ def perform_operation(num1, num2, operation):
         return num1 + num2
     elif operation == 'subtraction':
         return num1 - num2
-    elif operation == 'multiplication':
+    elif operation == 'multiply':
         return num1 * num2
     elif operation == 'division':
         if num2 == 0:
@@ -18,7 +18,7 @@ def main():
     try:
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
-        operation = input("Enter the operation (add, subtraction, multiplication, division): ")
+        operation = input("Enter the operation (add, subtraction, multiply, division): ")
 
         result = perform_operation(num1, num2, operation)
         print(f"Result: {result}")
